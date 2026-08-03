79da89b4454bb6200e828c8f2f4b2e20a490dbe4
Igor <igor.lsilva3110@gmail.com>
2026-08-03 11:06:09 -0300

    feat: adicionando página Home com formulário de cadastro de produto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import { useProduct, useProducts, useAddProduct } from "../../hooks/useProducts";
+import { ProductForm } from "../../components/ProductForm";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./style.css";
+
+export const Home = () => {
+  const { create } = useAddProduct();
+  const { loadProducts } = useProducts();
+
+  const handleCreate = async (data) => {
+    await create(data);
+    loadProducts();
+  };
+
+  return (
+    <>
+      <main className="home">
+        <ProductForm onSubmit={handleCreate} />
+        <div className="link-container">
+          <Link className="products-link" to="/produtos">
+            Veja os seus produtos
+          </Link>
+        </div>
+      </main>
+    </>
+  );
+};
